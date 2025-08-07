1f27232acb10080af8b621b6c19b99e08275e3c8
ardsn <rod_sn@hotmail.com>
2025-08-06 23:19:51 -0300

    improvements

--- src/ai_agent/graph.py
+++ src/ai_agent/graph.py
@@ -1,21 +1,19 @@
 import os
 from langchain_core.tools import tool
 from langchain_core.documents import Document
-# from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
 from langchain.chat_models import init_chat_model
 from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import MemorySaver
 from langchain_community.utilities import SQLDatabase
-# from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+import chromadb
 import httpx
 
 
-db = SQLDatabase.from_uri("sqlite:////home/antonio/Documentos/repos/project-api/db.sqlite3")
-# embeddings = HuggingFaceEmbeddings(
-#     model_name="sentence-transformers/all-mpnet-base-v2"
-# )
-# vector_store = InMemoryVectorStore(embeddings)
+db = SQLDatabase.from_uri("sqlite:////home/antonio/Documentos/repos/project-backend/db.sqlite3")
+chroma_client = chromadb.PersistentClient(path="/home/antonio/Documentos/repos/project-backend/.chroma")
+vector_store = Chroma(collection_name="001", client=chroma_client)
 llm = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
 memory = MemorySaver()
 toolkit = SQLDatabaseToolkit(db=db, llm=llm)
@@ -36,10 +34,16 @@ Quando for consultar dias e horários disponíveis,
 utilize as tools de SQL.
 
 Quando for realizar um agendamento, use a tool
-'create_appointment'. 
+'create_appointment'. Nunca agende um atendimento
+sem antes verificar a disponibilidade do dia e horário.
 
 ## Instruções para consulta de dias e horários disponíveis
  
+Antes de consultar algo no banco SQL, você deve sempre listar
+as tabelas disponíveis para decidir quais consultar. Em seguida
+deve consultar o schema das tabelas relevantes. Não pule
+este passo.
+
 Para checar dias e horários disponíveis para agendamentos, interaja
 com o banco de dados SQL. Para fazer uma consulta, crie uma query
 sintaticamente correta em {dialect} e veja os resultados da query.
@@ -57,11 +61,6 @@ ao executar uma query, reescreva a query e tente novamente.
 Não faça nenhuma declaração DML (INSERT, UPDATE, DELETE, DROP etc.) no banco
 de dados.
 
-Antes de consultar algo no banco SQL, você deve sempre listar
-as tabelas disponíveis para decidir quais consultar. Em seguida
-deve consultar o schema das tabelas relevantes. Não pule
-este passo.
-
 ## Instruções para realizar agendamento
 
 Você deve ter como input do cliente o seu CPF, nome completo,
@@ -72,17 +71,11 @@ dados sensíveis que não sejam do cliente.
     top_k=3,
 )
 
-# TODO: review this tool
-@tool(response_format="content_and_artifact")
-def retrieve(query: str) -> tuple[str, list[Document]]:
+@tool(response_format="content")
+def retrieve(query: str) -> str:
     """Retrieve information related to a query."""
-    # retrieved_docs: list[Document] = vector_store.similarity_search(query, k=2)
-    # serialized: str = "\n\n".join(
-    #     (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
-    #     for doc in retrieved_docs
-    # )
-    # return serialized, retrieved_docs
-    return "teste", []
+    result: str = vector_store.similarity_search(query, k=1)[0].page_content
+    return result
 
 # TODO: finalize this tool
 @tool(response_format="content_and_artifact")
@@ -117,9 +110,6 @@ agent_executor = create_react_agent(
 
 # Specify an ID for the thread
 config = {"configurable": {"thread_id": "abc123"}}
-
-# TODO: remove this
-# question = "Which country's customers spent the most?"
 
 while True:
     user_input = input("Você: ")
